01d91f50b650dedfb209c3a47e82edd1b6d16a33
Tomas Fagerbekk <tomas.fagerbekk@ntnu.no>
2013-12-11 19:46:43 +0100

    v 1.0

---
+++ focus.js
@@ -0,0 +1,48 @@
+/*
+*  Tomas Albertsen, 2013
+*  https://github.com/tomfa/focus
+* 
+*  Sets focus on the element marked with id=focuselement, or an appropriate
+*  element within it.
+*
+*  Dependencies: jQuery (v2.0.3)
+*
+*/
+
+$( document ).ready(function() {
+    // Defines
+    var focuselement = $('#focuselement');
+
+    // Type of elements we do not wish to focus .
+    // If the element is of any of these types, we use the search function
+    // to find an appropriate element within this one.
+    var wrongTypes = ["TD", "TH", "TABLE", "DIV", "P", "FORM", "UL", "LI"];
+
+    // If no focus element is found, we don't do anything
+    if (!(focuselement.length))
+        return;
+    
+    // If the focus element is of the wrong type, we exchange it with an 
+    // appropriate one
+    var type = getElementType(focuselement);
+    if (wrongTypes.indexOf(type) != -1)
+        focuselement = findInnerFocusElement(focuselement);
+
+    focuselement.focus();
+    
+
+    function getElementType(element) {
+        // Returns the type of the element
+        var type = element.type;
+        if (type === undefined)
+            type = element.prop('tagName');
+        return type;
+    }
+
+    function findInnerFocusElement (outer_element) {
+        // Returns an appropriate focus element within the given element
+        return $(outer_element).find('a,input[type=text],textarea,select'
+            ).filter(':visible').first();
+    }
+
+});
